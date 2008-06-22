9942d5dcbbd8c35b2a4af697719150fd09664f0d
Mark West <mark@markwest.me.uk>
2008-06-22 17:55:09 +0000

    added some interface improvements
    git-svn-id: file:///home/trac/svn/advancedpolls/trunk/advanced_polls@72 77ba05cd-ce11-0410-9b98-fcddaf3ce3ef

---
+++ pnjavascript/ap_admin_newmodify.js
@@ -0,0 +1,41 @@
+/*
+ *  $Id: news_admin_modifyconfig.js 23915 2008-03-09 10:40:46Z rgasch $ 
+ */
+Event.observe(window, 'load', ap_newmodify_init_check, false);
+
+function ap_newmodify_init_check()
+{
+    Event.observe('advancedpolls_noclosedate', 'click', ap_newmodify_closedate);
+    Event.observe('advancedpolls_recurring', 'click', ap_newmodify_recurring);
+	Event.observe('advancedpolls_multipleselect', 'click', ap_newmodify_multipleselect);
+	ap_newmodify_closedate();
+	ap_newmodify_recurring();
+	ap_newmodify_multipleselect();
+}
+
+function ap_newmodify_closedate()
+{
+    if($('advancedpolls_noclosedate').checked == true) {
+        $('advancedpolls_closedate_container').hide();
+    } else {
+        $('advancedpolls_closedate_container').show();
+    }
+}
+
+function ap_newmodify_recurring()
+{
+    if($('advancedpolls_recurring').getValue() == 0) {
+        $('advancedpolls_recurring_container').hide();
+    } else {
+        $('advancedpolls_recurring_container').show();
+    }
+}
+
+function ap_newmodify_multipleselect()
+{
+    if($('advancedpolls_multipleselect').getValue() == 0) {
+        $('advancedpolls_multipleselect_container').hide();
+    } else {
+        $('advancedpolls_multipleselect_container').show();
+	}
+}
