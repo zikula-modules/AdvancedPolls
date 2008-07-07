7e67e6cc0c68333a4aeb2b43808f8afbf7244bd2
Mark West <mark@markwest.me.uk>
2008-07-07 13:11:10 +0000

    language file tidying
    git-svn-id: file:///home/trac/svn/advancedpolls/trunk/advanced_polls@88 77ba05cd-ce11-0410-9b98-fcddaf3ce3ef

--- pnlang/eng/admin.php
+++ pnlang/eng/admin.php
@@ -11,102 +11,102 @@
  * @subpackage Advanced_Polls
  */
 
-define('_ADVANCEDPOLLSCANCELDELETE', 'Cancel deletion');
-define('_ADVANCEDPOLLSCONFIRMDELETE', 'Confirm deletion of Poll');
-if (!defined('_ADVANCEDPOLLSCREATEFAILED')) {
-    define('_ADVANCEDPOLLSCREATEFAILED', 'Creation attempt failed');
-}
-if (!defined('_ADVANCEDPOLLSDELETEFAILED')) {
-    define('_ADVANCEDPOLLSDELETEFAILED', 'Deletion attempt failed');
-}
-if (!defined('_ADVANCEDPOLLSUPDATEFAILED')) {
-    define('_ADVANCEDPOLLSUPDATEFAILED', 'Updating attempt failed');
-}
-define('_ADVANCEDPOLLSDELETE', 'Delete Poll');
-define('_ADVANCEDPOLLSEDIT', 'Edit Poll');
-define('_ADVANCEDPOLLSEDITCONFIG', 'Edit Polls Configuration');
-define('_ADVANCEDPOLLSNEW', 'New Poll');
+// main admin menu
 define('_ADVANCEDPOLLSADMIN', 'Advanced Polls Administration');
-define('_ADVANCEDPOLLSADD', 'Create Poll');
-define('_ADVANCEDPOLLSCREATED', 'Poll created');
-define('_ADVANCEDPOLLSDELETED', 'Poll deleted');
-define('_ADVANCEDPOLLSDISPLAYBOLD', 'Display item names in bold');
-define('_ADVANCEDPOLLSNAME', 'Name of Poll');
-define('_ADVANCEDPOLLSNOSUCHITEM', 'No such item');
-define('_ADVANCEDPOLLSNUMBER', 'Poll ID');
-define('_ADVANCEDPOLLSOPTIONS', 'Options');
-define('_ADVANCEDPOLLSUPDATE', 'Update Poll');
-define('_ADVANCEDPOLLSUPDATED', 'Poll updated');
+define('_ADVANCEDPOLLSNEW', 'New Poll');
 define('_ADVANCEDPOLLSVIEW', 'View Polls');
-define('_ADVANCEDPOLLSITEMSPERPAGE', 'Number of items per page');
+
+// delete template
+define('_ADVANCEDPOLLSCONFIRMDELETE', 'Confirm deletion of Poll');
+define('_ADVANCEDPOLLSDELETE', 'Delete Poll');
+
+// create/edit templates
+define('_ADVANCEDPOLLSADD', 'Create Poll');
+define('_ADVANCEDPOLLSAUTHTYPE', 'Poll Authorisation Method');
+define('_ADVANCEDPOLLSBASICINFO', 'Basic Information');
 define('_ADVANCEDPOLLSDATETIMESTART', 'Date and Time Poll Opens');
 define('_ADVANCEDPOLLSDATETIMECLOSE', 'Date and Time Poll Closes');
-define('_ADVANCEDPOLLSNEXT', 'Next');
-define('_ADVANCEDPOLLSRECURRING','Recurring Poll?');
-define('_ADVANCEDPOLLSRECURRINGOFFSET','Number of hours Poll reopens after');
-if (!defined('_LANGUAGE')) {
-    define('_LANGUAGE', 'Languages Poll is available in');
-}
-define('_ADVANCEDPOLLSVARIABLEERROR', 'Variable Error Will Robinson');
-define('_ADVANCEDPOLLSAUTHTYPE', 'Poll Authorisation Method');
+define('_ADVANCEDPOLLSDESCRIPTION', 'Description');
+define('_ADVANCEDPOLLSEDIT', 'Edit Poll');
 define('_ADVANCEDPOLLSMULTIPLESELECT', 'Selection Method');
 define('_ADVANCEDPOLLSMULTIPLESELECTCOUNT', 'Number of Selections Allowed');
 define('_ADVANCEDPOLLSMULTIPLESELECTCOUNTUNLIMITED', '(-1 for Unlimited Selections)');
-define('_ADVANCEDPOLLSTIEBREAK', 'Tiebreak Method');
-define('_ADVANCEDPOLLSRECURRINGINTERVAL', 'Number of Days Poll will recur after');
+define('_ADVANCEDPOLLSNAME', 'Name of Poll');
+define('_ADVANCEDPOLLSNOCLOSE', 'No close date');
 define('_ADVANCEDPOLLSONLYMULTIPLESELECT', 'Following option only relevant if a multiple selection poll is selected');
 define('_ADVANCEDPOLLSONLYRECURRING', 'Following option only relevant if a recurring poll is selected');
-define('_ADVANCEDPOLLSRESETVOTES', 'Reset Votes');
+define('_ADVANCEDPOLLSOPTION', 'Poll Option');
+define('_ADVANCEDPOLLSOPTIONCOUNT', 'Number of options in this poll');
+define('_ADVANCEDPOLLSOPTIONS', 'Options');
+define('_ADVANCEDPOLLSRECURRING','Recurring Poll?');
+define('_ADVANCEDPOLLSRECURRINGINTERVAL', 'Number of Days Poll will recur after');
+define('_ADVANCEDPOLLSRECURRINGOFFSET','Number of hours Poll reopens after');
+define('_ADVANCEDPOLLSREGULATIONS', 'Voting regulations');
+define('_ADVANCEDPOLLSTIEBREAK', 'Tiebreak Method');
+define('_ADVANCEDPOLLSTIMING', 'Timing');
+
+// view template
 define('_ADVANCEDPOLLSADMINSTATS', 'Voting Statistics');
-define('_ADVANCEDPOLLSVOTEID', 'Vote ID');
-define('_ADVANCEDPOLLSVOTEIP', 'IP Address');
-define('_ADVANCEDPOLLSVOTETIME', 'Time');
-define('_ADVANCEDPOLLSVOTEUID', 'Username');
-define('_ADVANCEDPOLLSVOTERANK', 'Vote Rank');
-define('_ADVANCEDPOLLSVOTEOPTIONID', 'Option');
-define('_ADVANCEDPOLLSADMINVOTINGSTATISTICS', 'Voting Statistics');
-define('_ADVANCEDPOLLSADMINVOTINGSTATISTICSTABLE', 'Vote History');
-define('_ADVANCEDPOLLSRESETVOTESCONFIRM', 'Reset Votes'); 
-define('_ADVANCEDPOLLSCONFIRMVOTESRESET', 'Cofirm Reset of Votes'); 
-define('_ADVANCEDPOLLSCANCELVOTESREST', 'Cancel Reset');
-define('_ADVANCEDPOLLSVOTESRESET', 'Votes Reset');
-define('_ADVANCEDPOLLSVOTESRESETFAILED', 'Vote reset failed');
+define('_ADVANCEDPOLLSDUPLICATE', 'Duplicate Poll'); 
+define('_ADVANCEDPOLLSNUMBER', 'Poll ID');
+define('_ADVANCEDPOLLSRESETVOTES', 'Reset Votes');
+
+// modify config template
+define('_ADVANCEDPOLLSADMINITEMSPERPAGE', 'items per page in admin interface');
 define('_ADVANCEDPOLLSADMINDATEFORMAT', 'Date format for Admin Interface');
 define('_ADVANCEDPOLLSUSERDATEFORMAT', 'Date format for User Interface');
-define('_ADVANCEDPOLLSUSEREVERSEDNS', 'Use Reverse DNS for IP Addresses');
+define('_ADVANCEDPOLLSDEFAULTCOLOUR', 'Default colour');
+define('_ADVANCEDPOLLSDEFAULTOPTIONCOUNT', 'Default number of options in a poll');
+define('_ADVANCEDPOLLSDISPLAYBOLD', 'Display item names in bold');
 define('_ADVANCEDPOLLSSCALINGFACTOR', 'Scaling Factor for Poll Results Bar'); 
-define('_ADVANCEDPOLLSDUPLICATE', 'Duplicate'); 
+define('_ADVANCEDPOLLSUSEREVERSEDNS', 'Use Reverse DNS for IP Addresses');
+define('_ADVANCEDPOLLSUSERITEMSPERPAGE', 'items per page in user interface');
+
+// reset votes template
+define('_ADVANCEDPOLLSCONFIRMVOTESRESET', 'Cofirm Reset of Votes'); 
+define('_ADVANCEDPOLLSRESETVOTESCONFIRM', 'Reset Votes'); 
+
+// duplicate poll template
 define('_ADVANCEDPOLLSCONFIRMDUPLICATE', 'Confirm Duplication of Poll');
 define('_ADVANCEDPOLLSCANCELDUPLICATE', 'Cancel Duplication of Poll');
-//define('_ADVANCEDPOLLSDUPLICATE', 'Duplicate Poll');
+
+// voting statistics template
+define('_ADVANCEDPOLLSVOTECOUNT', 'Vote count');
+define('_ADVANCEDPOLLSSORTVOTES', 'Sort Votes');
 define('_ADVANCEDPOLLSSORTVOTESBY', 'Sort Votes by');
 define('_ADVANCEDPOLLSSORTVOTESORDER', 'Sort Order');
-define('_ADVANCEDPOLLSSORTVOTES', 'Sort Votes');
-define('_ADVANCEDPOLLSSORTASCENDING', 'Ascending');
-define('_ADVANCEDPOLLSSORTDESCENDING', 'Descending');
-define('_ADVANCEDPOLLSOPTION', 'Poll Option');
-define('_ADVANCEDPOLLSNOAUTH', 'Not Authorised to Access Advanced Polls Module');
-define('_ADVANCEDPOLLSDUPLICATED', 'Poll Duplicated');
-define('_ADVANCEDPOLLSFREE', 'Free');
-define('_ADVANCEDPOLLSUSERID', 'User ID');
+define('_ADVANCEDPOLLSVOTEID', 'Vote ID');
+define('_ADVANCEDPOLLSVOTEIP', 'IP Address');
+define('_ADVANCEDPOLLSVOTEOPTIONID', 'Option');
+define('_ADVANCEDPOLLSVOTERANK', 'Vote Rank');
+define('_ADVANCEDPOLLSVOTETIME', 'Time');
+define('_ADVANCEDPOLLSVOTEUID', 'Username');
+define('_ADVANCEDPOLLSADMINVOTINGSTATISTICS', 'Voting Statistics');
+define('_ADVANCEDPOLLSADMINVOTINGSTATISTICSTABLE', 'Vote History');
+
+// voting regulations drop down
 define('_ADVANCEDPOLLSCOOKIE', 'Cookie');
+define('_ADVANCEDPOLLSFREE', 'Free');
 define('_ADVANCEDPOLLSIPADDRESS', 'IP Address');
+define('_ADVANCEDPOLLSUSERID', 'User ID');
+
+// tiebreak drop down
+define('_ADVANCEDPOLLSALPHABETICAL', 'Alphabetical');
 define('_ADVANCEDPOLLSNONE', 'None');
 define('_ADVANCEDPOLLSVOTETIMECOUNTBACK', 'Vote Time Count Back');
-define('_ADVANCEDPOLLSALPHABETICAL', 'Alphabetical');
+
+// multiple select drop down
 define('_ADVANCEDPOLLSSINGLE', 'Single');
 define('_ADVANCEDPOLLSMULTIPLE', 'Multiple');
 define('_ADVANCEDPOLLSRANKED', 'Ranked');
-define('_ADVANCEDPOLLSADMINITEMSPERPAGE', 'items per page in admin interface');
-define('_ADVANCEDPOLLSUSERITEMSPERPAGE', 'items per page in user interface');
-define('_ADVANCEDPOLLSDEFAULTCOLOUR', 'Default colour');
-define('_ADVANCEDPOLLSDEFAULTOPTIONCOUNT', 'Default number of options in a poll');
-define('_ADVANCEDPOLLSOPTIONCOUNT', 'Number of options in this poll');
-define('_ADVANCEDPOLLNOSUCHITEM', 'No such poll');
-define('_ADVANCEDPOLLSVOTECOUNT', 'Vote count');
-define('_ADVANCEDPOLLSDESCRIPTION', 'Description');
-define('_ADVANCEDPOLLSNOCLOSE', 'No close date');
-define('_ADVANCEDPOLLSBASICINFO', 'Basic Information');
-define('_ADVANCEDPOLLSTIMING', 'Timing');
-define('_ADVANCEDPOLLSREGULATIONS', 'Voting regulations');
 
+// sort order drop down
+define('_ADVANCEDPOLLSSORTASCENDING', 'Ascending');
+define('_ADVANCEDPOLLSSORTDESCENDING', 'Descending');
+
+// error status messages
+define('_ADVANCEDPOLLSCREATED', 'Poll created');
+define('_ADVANCEDPOLLSDELETED', 'Poll deleted');
+define('_ADVANCEDPOLLSDUPLICATED', 'Poll Duplicated');
+define('_ADVANCEDPOLLSUPDATED', 'Poll updated');
+define('_ADVANCEDPOLLSVOTESRESETFAILED', 'Vote reset failed');
